555113828bcbc26c42e591e231cd535c99042cf1
sam wunderilch <swund@MacBook-Pro.local>
2020-07-19 10:01:15 -0400

    submit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # aloha
 module 9
+###challenge suggestions
+
+December has nearly 2x the average rain by days.  Also, based on the STD.dev being much higher in December we can infer that it rains less days - but rains more.  
+
+To really make a determination if sales traffic will be different in June vs Dec. I would suggest that we also bring in additional additional weather data - specifically temp.  This would help us better understand the 'feel' for the weather and help us better understand if its ice cream weather.  In addition, if traffic patterns were available this would be invaluable data to estimate the total foot traffic in the area.
+
+Lastly, I would suggest that we not only look at the data as a summary by month - but we could chart out the data daily within the month.  the overlay for the temp and rain by day would help me visualize the data to better understand common trends.  The statistical data on its own is almost to general to really get a good grasp of weather.
+
+
+P.S.  the challenge wasn't explicit so I choose to summarize the data by the months for all years data present.  I know this could present some additional scenarios, like if we have more data for one year over another based on when the dataset started... but, as its an average anyway I thought 'smoothing' out the data was more effective than filtering the data set to contain the exact # of values in each month....  I also chose not to group by year as we weren't investigating any deviation on the monthly averages across the years.  But, that could be a good exercise also to see if there are any years in the data that stand out.
--- app.py
+++ app.py
@@ -17,11 +17,11 @@ session = Session(engine)
 
 app = Flask(__name__)
 
-@app.route("/")
 
+@app.route("/")
 def welcome():
     return(
-    '''
+        '''
     Welcome to the Climate Analysis API!
     Available Routes:
     /api/v1.0/precipitation
@@ -30,19 +30,22 @@ def welcome():
     /api/v1.0/temp/start/end
     ''')
 
+
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-   precipitation = session.query(Measurement.date, Measurement.prcp).\
-	filter(Measurement.date >= prev_year).all() 
-   precip = {date: prcp for date, prcp in precipitation}
-   return jsonify(precip)
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
 
 @app.route("/api/v1.0/stations")
 def stations():
     results = session.query(Station.station).all()
     stations = list(np.ravel(results))
     return jsonify(stations=stations)
+
 
 @app.route("/api/v1.0/tobs")
 def temp_monthly():
@@ -55,33 +58,22 @@ def temp_monthly():
     return jsonify(temps=temps)
 
 @app.route("/api/v1.0/temp/<start>")
-def stats(start=None, end=None):
-    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]           
-
-if not end: 
-    results = session.query(*sel).\
-        filter(Measurement.date <= start).all()
-    temps = list(np.ravel(results))
-    return jsonify(temps)
-
-    results = session.query(*sel).\
-            filter(Measurement.date >= start).\
-        filter(Measurement.date <= end).all()
-    temps = list(np.ravel(results))
-    return jsonify(temps=temps)
- 
 @app.route("/api/v1.0/temp/<start>/<end>")
 def stats(start=None, end=None):
-    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]           
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
 
-if not end: 
-    results = session.query(*sel).\
+    if not end:
+        results = session.query(*sel).\
         filter(Measurement.date <= start).all()
-    temps = list(np.ravel(results))
-    return jsonify(temps)
+        temps = list(np.ravel(results))
+        return jsonify(temps)
 
     results = session.query(*sel).\
-            filter(Measurement.date >= start).\
+        filter(Measurement.date >= start).\
         filter(Measurement.date <= end).all()
     temps = list(np.ravel(results))
     return jsonify(temps=temps)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
